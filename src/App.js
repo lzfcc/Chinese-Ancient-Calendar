c24cc15c7e5e3016f44f4dbef6451b4f0642e018
kujihhoe <kujihhoe@gmail.com>
2021-02-23 15:38:23 +0800

    修改css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React from 'react';
 import './App.css'
-import calculate from "../src/Shangshu-calendar";
-import { CalNameList } from "../src/Shangshu-calendar/constant"
+import calculate from '../src/Shangshu-calendar';
+import { CalNameList } from '../src/Shangshu-calendar/constant'
+import SCList from '../src/Shangshu-calendar/constant'
 
-const tableRowNameMap = {
-  MonthPrint: '月',
+const TableRowNameMap = {
+  MonthPrint: '月序',
+  NewmAvgSCPrint: '平朔',
+  NewmAvgDecimalPrint: '分',
   NewmSCPrint: '定朔',
-  NewmMmddPrint: '日期',
+  NewmMmddPrint: '公曆',
   NewmDecimal3Print: '三次',
   NewmDecimal2Print: '二次',
   NewmDecimal1Print: '一次',
-  NewmAvgSCPrint: '平朔',
-  NewmAvgDecimalPrint: '分',
-  NewmAvgSCPrint: '平朔',
-  NewmMmddPrint: '日期',
-  NewmAvgDecimalPrint: '分',
   SyzygySCPrint: '望',
   SyzygyDecimalPrint: '分',
   TermNamePrint: '中氣',
@@ -31,79 +29,81 @@ export default class extends React.Component {
     this.handleRetrieve = this.handleRetrieve.bind(this);
 
     this.renderTableList = this.renderTableList.bind(this);
-    this.renderTableContent = this.renderTableContent.bind(this);
+    this.RenderTableContent = this.RenderTableContent.bind(this);
     this.renderMode = this.renderMode.bind(this);
     this.renderInput = this.renderInput.bind(this);
     this.renderCalendar = this.renderCalendar.bind(this);
 
     this.state = {
-      calendar: "Yin",
-      mode: "0",
-      yearStart: "",
-      yearEnd: "",
-      output: ""
+      calendar: 'Yin',
+      mode: '0',
+      YearStart: '',
+      yearEnd: '',
+      output: ''
     };
   }
 
   handleRetrieve(e) {
-    const yearStart = Number(this.state.yearStart);
+    const YearStart = Number(this.state.YearStart);
     const yearEnd = Number(this.state.yearEnd);
     let result = [];
-    if (this.state.mode === "0") {
-      console.log(this.state.yearStart, yearStart);
-      if (this.state.yearStart.length === 0 || Number.isNaN(yearStart)) {
-        alert("输入年份不合法！" + this.state.calendar);
+    if (this.state.mode === '0') {
+      console.log(this.state.YearStart, YearStart);
+      if (this.state.YearStart.length === 0 || Number.isNaN(YearStart)) {
+        alert('输入年份不合法！' + this.state.calendar);
         return;
       }
 
-      result.push(calculate(this.state.calendar, yearStart))
+      result.push(calculate(this.state.calendar, YearStart))
     } else {
       if (
-        this.state.yearStart.length === 0 ||
-        Number.isNaN(yearStart) ||
+        this.state.YearStart.length === 0 ||
+        Number.isNaN(YearStart) ||
         this.state.yearEnd.length === 0 ||
         Number.isNaN(yearEnd)
       ) {
-        alert("输入年份不合法！");
+        alert('输入年份不合法！');
         return;
       }
-      if (yearStart > yearEnd) {
-        alert("起始年份不可以大于终止年份！");
+      if (YearStart > yearEnd) {
+        alert('起始年份不可以大于终止年份！');
         return;
       }
       // alert(this.state.year);
-      for (let y = yearStart; y <= yearEnd; ++y) {
+      for (let y = YearStart; y <= yearEnd; ++y) {
         result.push(calculate(this.state.calendar, y));
       }
     }
     this.setState({ output: result });
+   
   }
 
   renderTableList() {
     return (
       <div>
-        {(this.state.output || []).map((calData) => {
-         return (<table>
-            <tr><th colSpan={13}>{calData.YearInfo}</th></tr>
-            {this.renderTableContent(calData)}
-          </table>)
+        {(this.state.output || []).map((CalData) => {
+         return (<div class='single-cal'>           
+           <p>{CalData.YearInfo}</p>
+           <table>
+            <tr> {this.RenderTableContent(CalData)}</tr>
+          </table></div>)
         })}
       </div>
     );
   }
 
-  renderTableContent (calData) {
-    return Object.entries(calData).map(([key, value]) => {
-      let row = null
+  RenderTableContent (CalData) {
+    return Object.entries(CalData).map(([key, value]) => {
       if (Array.isArray(value) && value.length > 0) {
-        row = [<td>{tableRowNameMap[key]}</td>].concat(value.map((x) => (<td>{x}</td>)))
+        return <tr className={key}>{
+         [<th>{TableRowNameMap[key]}</th>].concat(value.map((x) => (<td>{x}</td>)))
+         }</tr>
       }
-      return (<tr>
-        {row}
-      </tr>)
-    })
+      return null
+     })
   }
 
+  
   renderMode() {
     return (
       <div
@@ -112,12 +112,12 @@ export default class extends React.Component {
         }}
       >
         <span>
-          <input type="radio" name="retrieve-mode" value="0" defaultChecked />
-          <label for="0">特定年份</label>
+          <input type='radio' name='retrieve-mode' value='0' defaultChecked />
+          <label for='0'>特定年</label>
         </span>
         <span>
-          <input type="radio" name="retrieve-mode" value="1" />
-          <label for="1">年份区间</label>
+          <input type='radio' name='retrieve-mode' value='1' />
+          <label for='1'>年區閒</label>
         </span>
       </div>
     );
@@ -125,17 +125,17 @@ export default class extends React.Component {
 
   renderInput() {
     return (
-      <span className="year-select">
-        <span>（1=公元1年，0=公元前1年，-1=公元前2年）</span>
+      <span class='year-select'>
+        <span class='note'>（1=公元1年，0=公元前1年，-1=公元前2年）</span>
         <input
           onChange={(e) => {
-            this.setState({ yearStart: e.currentTarget.value });
+            this.setState({ YearStart: e.currentTarget.value });
           }}
         />
         <span>年</span>
-        {this.state.mode === "1" ? (
-          <span className="year-end">
-            <span>至</span>
+        {this.state.mode === '1' ? (
+          <span class='year-end'>
+            <span>—</span>
             <input
               onChange={(e) => {
                 this.setState({ yearEnd: e.currentTarget.value });
@@ -150,7 +150,7 @@ export default class extends React.Component {
 
   renderCalendar() {
     return (
-      <div className="calendar-select">
+      <div class='calendar-select'>
         <select
           value={this.state.calendar}
           onChange={(e) => {
@@ -167,7 +167,7 @@ export default class extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div class='App'>
         {this.renderMode()}
         {this.renderCalendar()}
         {this.renderInput()}
