3ffa6fe2d5db98eb4d03f3b72520c1d1e77e6f46
kujihhoe <kujihhoe@gmail.com>
2021-03-16 20:23:07 +0800

    增加小數連分數

--- src/Converter.js
+++ src/Converter.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { DecomposePrimeFactor } from '../src/Shangshu-calendar/convert_decompose-prime-factor'
-import { CongruenceModulo, ContinuedFrac } from '../src/Shangshu-calendar/convert_congruence-modulo'
+import { CongruenceModulo, ContinuedFrac, ContinuedFrac1 } from '../src/Shangshu-calendar/convert_congruence-modulo'
 import { Jd2Date1, Date2Jd } from '../src/Shangshu-calendar/convert_jd2date'
 import MathJax from './Mathjax'
+
 export default class Converter extends React.Component {
   constructor (props) {
     super(props)
@@ -11,6 +12,7 @@ export default class Converter extends React.Component {
       b: '',
       bigNumer: 26,
       bigDenom: 49,
+      Times: 50,
       aRaw: '',
       bRaw: '',
       title: '弱率',
@@ -18,22 +20,28 @@ export default class Converter extends React.Component {
       yy: '',
       mm: '',
       dd: '',
+      h: '0',
+      m: '0',
+      s: '0',
+      ms: '000',
       outputModulo: null,
       outputDecompse: null,
       outputContinuedFrac: null,
+      outputContinuedFrac1: null,
       outputJd: null,
       outputDate: null,
     }
     this.handleConvertModulo = this.handleConvertModulo.bind(this)
     this.handleConvertDecompose = this.handleConvertDecompose.bind(this)
     this.handleConvertContinuedFrac = this.handleConvertContinuedFrac.bind(this)
+    this.handleConvertContinuedFrac1 = this.handleConvertContinuedFrac1.bind(this)
     this.handleConvertJd = this.handleConvertJd.bind(this)
     this.handleConvertDate = this.handleConvertDate.bind(this)
   }
 
   renderConverterInputModulo () {
     return (
-      <span className='year-select Decompose'>
+      <span className='year-select width3'>
         <span>鍵入兩箇互質整數</span>
         <input
           value={this.state.aRaw}
@@ -53,7 +61,7 @@ export default class Converter extends React.Component {
 
   renderConverterInputContinuedFrac () {
     return (
-      <span className='year-select Decompose'>
+      <span className='year-select width3'>
         <span>分子</span>
         <input
           value={this.state.fracA}
@@ -72,9 +80,31 @@ export default class Converter extends React.Component {
     );
   }
 
+  renderConverterInputContinuedFrac1 () {
+    return (
+      <span className='year-select width4'>
+        <span>小數</span>
+        <input
+          value={this.state.Decimal}
+          onChange={(e) => {
+            this.setState({ Decimal: e.currentTarget.value });
+          }}
+        />
+        {/* <span> 漸進次數</span>
+        <input
+          value={this.state.Times}
+          onChange={(e) => {
+            this.setState({ Times: e.currentTarget.value });
+          }}
+        /> */}
+      </span>
+    );
+  }
+
+
   renderConverterInputDecompose () {
     return (
-      <span className='year-select Decompose'>
+      <span className='year-select width3'>
         <span>朔餘</span>
         <input
           value={this.state.a}
@@ -111,7 +141,7 @@ export default class Converter extends React.Component {
     return (
       <span className='year-select'>
         <span>儒略日</span>
-        <input className='InputJd'
+        <input className='width4'
           value={this.state.jd}
           onChange={(e) => {
             this.setState({ jd: e.currentTarget.value });
@@ -124,27 +154,55 @@ export default class Converter extends React.Component {
   renderConverterInputDate () {
     return (
       <span className='year-select'>
-        <input className='InputDate'
+        <input className='width2'
           value={this.state.yy}
           onChange={(e) => {
             this.setState({ yy: e.currentTarget.value });
           }}
         />
         <span>年</span>
-        <input className='InputDate'
+        <input className='width1'
           value={this.state.mm}
           onChange={(e) => {
             this.setState({ mm: e.currentTarget.value });
           }}
         />
         <span>月</span>
-        <input className='InputDate'
+        <input className='width1'
           value={this.state.dd}
           onChange={(e) => {
             this.setState({ dd: e.currentTarget.value });
           }}
         />
         <span>日</span>
+        <input className='width1'
+          value={this.state.h}
+          onChange={(e) => {
+            this.setState({ h: e.currentTarget.value });
+          }}
+        />
+        <span>h</span>
+        <input className='width1'
+          value={this.state.m}
+          onChange={(e) => {
+            this.setState({ m: e.currentTarget.value });
+          }}
+        />
+        <span>m</span>
+        <input className='width1'
+          value={this.state.s}
+          onChange={(e) => {
+            this.setState({ s: e.currentTarget.value });
+          }}
+        />
+        <span>s</span>
+        <input className='width2'
+          value={this.state.ms}
+          onChange={(e) => {
+            this.setState({ ms: e.currentTarget.value });
+          }}
+        />
+        <span>ms</span>
       </span>
     );
   }
@@ -162,16 +220,26 @@ export default class Converter extends React.Component {
   handleConvertContinuedFrac () {
     try {
       const { gcdPrint, z, zPrint, Result } = ContinuedFrac(this.state.fracA, this.state.fracB)
-      this.setState({ outputContinuedFrac: gcdPrint,outputContinuedFrac1: zPrint, outputContinuedFrac2: Result,outputContinuedFrac3: z }) //{this.state.outputContinuedFrac3}
+      this.setState({ outputContinuedFrac: gcdPrint,outputContinuedFrac1: zPrint, outputContinuedFrac2: Result,outputContinuedFrac3: z })
     } catch (e) {
         alert(e.message)
     }
   }
+
+  handleConvertContinuedFrac1 () {
+    try {
+      const { z, zPrint, Result } = ContinuedFrac1(this.state.Decimal, this.state.Times)
+      this.setState({ outputContinuedFrac11: zPrint, outputContinuedFrac12: Result, outputContinuedFrac13: z })
+    } catch (e) {
+        alert(e.message)
+    }
+  }
+
   convertLatex (nums) {
     let str = ''
     nums.reverse().forEach((x) => {
       if (!str) {
-        str = `1 \\over {${x}}`
+        str = `${x}`
       } else {
         str = `${x} + {1 \\over {${str}}}`
       }
@@ -182,7 +250,7 @@ export default class Converter extends React.Component {
   handleConvertDecompose () {
     try {
       const { SmallPrint, Result, Foot } = DecomposePrimeFactor(this.state.a, this.state.b, this.state.bigNumer, this.state.bigDenom)
-      this.setState({ outputDecompse: Result, outputDecompse1: SmallPrint, outputDecompse2: Foot  })
+      this.setState({ outputDecompse: Result, outputDecompse1: SmallPrint, outputDecompse2: Foot })
     } catch (e) {
       alert(e.message)
     }
@@ -200,7 +268,7 @@ export default class Converter extends React.Component {
 
   handleConvertDate () {
     try {
-      const { Result } = Date2Jd(this.state.yy, this.state.mm, this.state.dd)
+      const { Result } = Date2Jd(this.state.yy, this.state.mm, this.state.dd, this.state.h, this.state.m, this.state.s, this.state.ms)
       this.setState({ outputDate: Result })
     } catch (e) {
         alert(e.message)
@@ -225,9 +293,20 @@ export default class Converter extends React.Component {
     }
     return (
       <div>
-        <p>{this.state.outputContinuedFrac}</p>
-        <p>{this.state.outputContinuedFrac1}</p>
+        <p>{this.state.outputContinuedFrac}　　{this.state.outputContinuedFrac1}</p>
         <p>{this.state.outputContinuedFrac2}</p>
+      </div>
+    )
+  }
+
+  renderResultContinuedFrac1 () {
+    if (!this.state.outputContinuedFrac11) {
+      return null
+    }
+    return (
+      <div>
+        <p>{this.state.outputContinuedFrac11}</p>
+        <p>{this.state.outputContinuedFrac12}</p>
       </div>
     )
   }
@@ -284,11 +363,6 @@ export default class Converter extends React.Component {
     )
   }
 
-  componentDidMount() {    
-      const script = document.createElement('script');
-      script.src = "https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js";
-      script.async = true;
-      document.body.appendChild(script)}
   render () {
     return (
       <section>
@@ -301,11 +375,18 @@ export default class Converter extends React.Component {
         <div className='convert-div'>
         <h2>連分數逼近</h2>
         {this.renderConverterInputContinuedFrac()}
-        <button onClick={this.handleConvertContinuedFrac} className='button4-3'>衝衝衝 !</button>
-        {this.renderResultContinuedFrac()}
+        <button onClick={this.handleConvertContinuedFrac} className='button4-3'>快快快 !</button>
         {(this.state.outputContinuedFrac3 || []).length > 0 ?
             <MathJax rawLatex={this.convertLatex(this.state.outputContinuedFrac3)}/> : null
         }
+        {this.renderResultContinuedFrac()}
+        <p></p>
+        {this.renderConverterInputContinuedFrac1()}
+        <button onClick={this.handleConvertContinuedFrac1} className='button4-3'>衝衝衝 !</button>
+        {(this.state.outputContinuedFrac13 || []).length > 0 ?
+            <MathJax rawLatex={this.convertLatex(this.state.outputContinuedFrac13)}/> : null
+        }
+        {this.renderResultContinuedFrac1()}
         </div>
         <div className='convert-div'>
         <h2>調日法</h2>
